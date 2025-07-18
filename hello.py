f99e810d8d2ddb38d8ddf3cb33d4686cf6a5dacc
Mansur550 <mansurislam59@gmail.com>
2025-07-19 01:57:35 +0600

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,8 +1,24 @@
-#Ask user their name
-#Remove whitespace from str and Capitalized
-name = input("what is your name? ").strip().title()
+# #Ask user their name
+# #Remove whitespace from str and Capitalized
+# name = input("what is your name? ").strip().title()
+#
+# #split user's name into firstname and last name
+# first, last =name.split(" ")
+# #say hello to the user
+# print(f"hello, {first}" )
 
-#say hello to the user
-print(f"hello, {name}" )
+
+
+def main():
+    hello()
+    name = input("Whats your name? ")
+    hello(name)
+
+def hello(to="world"):
+    print("Hello!", to)
+
+
+
+main()
 
 
